c2132fe15e5b74275778faf4ee7885d7ea9d445c
maadesh124 <maadeshashok124@gmail.com>
2025-03-02 21:46:27 +0530

    src and dst removed

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,18 @@
 int main()
 {
     int m,n;
-    int src[2]={0,0};
-    int dst[2]={4,4};
+
     cout<<"Enter Maze dimensions"<<endl;
     cin>>m;
     cin>>n;
-    cout<<"Ente src";
-    cin>>src[0];
-    cin>>src[1];
-    cout<<"Enter dst";
-    cin>>dst[0];
-    cin>>dst[1];
+    int src[2]={0,0};
+    int dst[2]={m-1,n-1};
+    // cout<<"Ente src";
+    // cin>>src[0];
+    // cin>>src[1];
+    // cout<<"Enter dst";
+    // cin>>dst[0];
+    // cin>>dst[1];
     Maze* maze=new Maze(m,n,src,dst);
     //maze->startGame(src,dst);
 }
